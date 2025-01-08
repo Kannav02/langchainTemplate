c3dc2ac667f7f4f27b7744b6f7b195bd2767c5c9
Kannav02 <kannavsethi02@gmail.com>
2025-01-07 21:28:12 -0500

    initial files have been added

---
+++ langchain_utils.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from langchain.chains import SimpleSequentialChain,SequentialChain
+from dotenv import load_dotenv, dotenv_values
+import os
+
+
+config = dotenv_values(".env")
+openai_api_key = config["OPENAI_API_KEY"]
+print(openai_api_key)
+
+llm = OpenAI(api_key = openai_api_key, temperature = 0.6)
+
+story_template = PromptTemplate(input_variables = ["topic"],template = "Generate me a brief story based on this {topic}?")
+moral_template = PromptTemplate(input_variables=["story"],template = "What is the moral of the {story} ?")
+
+story_chain = story_template | llm
+moral_chain = moral_template | llm
+
+chain = SimpleSequentialChain(
+    chains = [story_chain,moral_chain],
+    input = 
+)
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hello World")
+
+randomPicker = st.selectbox("Pick a number", [1, 2, 3, 4, 5])
