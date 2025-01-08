affc9517d2d7f211a6e408a047557c1c1a011f6f
Kannav02 <kannavsethi02@gmail.com>
2025-01-07 22:54:52 -0500

    demonstrated the ways to handle simple sequential chaining for the same

--- langchain_utils.py
+++ langchain_utils.py
@@ -15,11 +15,14 @@ story_template = PromptTemplate(input_variables = ["topic"],template = "Generate
 moral_template = PromptTemplate(input_variables=["story"],template = "What is the moral of the {story} ?")
 
 story_chain = story_template | llm
-moral_chain = moral_template | llm
+story_result = story_chain.invoke({"topic":"The Punisher From Marvel Comics"})
 
-chain = SimpleSequentialChain(
-    chains = [story_chain,moral_chain],
-    input = 
-)
+moral_chain = moral_template | llm
+moral_result = moral_chain.invoke({"story":story_result})
+
+print(moral_result)
+
+
+
 
 
